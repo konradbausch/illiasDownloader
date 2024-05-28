fcf98f22662c4257c1f1f94e20621c1c3672f1af
Konrad Bausch <dev@konradbausch.com>
2024-05-28 13:37:58 +0200

    added documentation

---
+++ README.md
@@ -0,0 +1,20 @@
+# Illias-Downloader
+
+## Funtionality
+Can automaticly download all files from a illias folder and put them in the desired folder on your machine.
+
+## Limitations
+- doesn't copy folder structure of subfolders, only extracts the files and puts them all into one folder
+- only works for Illias of uni tuebingen, since other illias have different layouts
+- only works if Firefox is installed
+
+## Usage
+1. install Firefox, if you dont have it installed
+2. install requirements `pip install -r requirements.txt`
+3. go into the `download.sh` and input your username and password, also change the download dir and url.
+
+You can find out the download url by going to the folder and clicking on the folder again in the menu, as shown on the picture below.
+
+![image1](image.png)
+
+Then just copy the link from your browser and insert it.
--- download.sh
+++ download.sh
@@ -1,8 +1,13 @@
+#input your username and password
 username="YOUR USERNAME"
 password="YOUR PASSWORD"
 
+#optional, removes the folder before downloading all files again, so you dont get duplicates
 rm -rf /home/konrad/Uni/downloads
 
+
+#replace the download dir and url
+#downloads the files in parralel
 python main.py --download_dir /home/konrad/Uni/downloads/ml/ubs --url "https://ovidius.uni-tuebingen.de/ilias3/goto.php?target=fold_4640032&client_id=pr02" --username "$username" --password "$password" &
 python main.py --download_dir /home/konrad/Uni/downloads/m/ubs --url "https://ovidius.uni-tuebingen.de/ilias3/goto.php?target=fold_4566020&client_id=pr02" --username "$username" --password "$password" &
 python main.py --download_dir /home/konrad/Uni/downloads/theo/ubs --url "https://ovidius.uni-tuebingen.de/ilias3/goto.php?target=fold_4635382&client_id=pr02" --username "$username" --password "$password" &
